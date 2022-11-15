d4f57b706a0207d71edb5272422f0e741a5f444d
Sunil Kumar <skumar.mca2010@gmail.com>
2022-11-15 22:45:13 +0530

    Updated Profile

--- src/components/recent-projects/index.js
+++ src/components/recent-projects/index.js
@@ -1,157 +1,195 @@
+const responsibilities = [
+  'Proactively research and locate necessary tools and processes to identify troublesome trends as they develop.',
+  'Introduce new IT system design and testing procedures as well as quality standards to improve business performance, productivity and compliance across organization.',
+  'Provide expert application design, guidance on solution system designs, redesigns for platform, performance and integration of new technical features and capabilities.',
+  'Write clean, maintainable and efficient code.',
+  'Participate in the architecture and design of the feature'
+];
+
+const projectList = [
+  {
+    id: 12,
+    name: 'UI Geeks',
+    description:
+      'UI-Geeks is an online learning platform. Learn Core and Advanced Concepts, Blogs, Summary of JavaScript, React, Angular, SCSS, CSS.',
+    role: 'Owner',
+    techStack: ['ReactJs'],
+    link: 'https://ui-geeks.in'
+  },
+  {
+    id: 1,
+    name: 'File Exchange - XR',
+    description:
+      'File Exchange leverages four decades of transmission and translation expertise to provide the most robust data delivery and transformation tool available. With File Exchange, banks can eliminate the hassles of installing software, managing servers, designing networks.',
+    role: 'UI Architect',
+    techStack: ['ReactJs', 'Redux'],
+    rolesAndResponsibilities: responsibilities
+  },
+  {
+    id: 2,
+    name: 'Payments Exchange - RTP',
+    description:
+      'With the use of RTP, banks can now rapidly onboard new customer while providing existing customer an expanded suite of payments options, including real time payments.',
+    role: 'UI Architect',
+    techStack: ['ReactJs', 'Redux']
+  },
+  {
+    id: 3,
+    name: 'Enterprise Payment Platform',
+    description: 'EPP UI is the modern UI for handling all types of payments. ',
+    role: 'UI Architect',
+    techStack: ['ReactJs', 'Redux']
+  },
+  {
+    id: 4,
+    name: 'CSCS',
+    description:
+      'CSCS cards provide proof that individuals working on construction sites have the appropriate training and qualifications for the job they do on site. By ensuring the workforce are appropriately qualified the card plays its part in improving standards and safety on UK construction sites',
+    role: 'Software Architect',
+    techStack: ['Angular 8', 'NodeJs', 'AWS']
+  },
+  {
+    id: 5,
+    name: 'ASTM',
+    description:
+      'ASTM International, formerly known as American Society for Testing and Materials, is an international standards organization that develops and publishes voluntary consensus technical standards for a wide range of materials, products, systems, and services.',
+    role: 'UI Architect',
+    techStack: ['ReactJs', 'NodeJs']
+  },
+  {
+    id: 6,
+    name: 'MyBI',
+    description:
+      'MyBI Portal is a one stop web application which allows AT&T staff to search documents shared across multiple source systems. Users can also subscribe any document and if that document has any change, it gets notified to the user.',
+    role: 'Tech Lead, UI Specialist',
+    techStack: ['ReactJs', 'NodeJs']
+  },
+  {
+    id: 7,
+    name: 'CityNet',
+    description:
+      'NLT is one of the biggest Internet Service provider of Singapore. Users can request for new connection with their convenient time, and then they can track the progress of that request.',
+    role: 'Tech Lead, UI Specialist',
+    techStack: ['ReactJs', 'Redux']
+  },
+  {
+    id: 8,
+    name: 'XPO Logistics',
+    description:
+      'XPO Logistics is one of the largest logistics companies of US. This product is to provide a high-end system to enhance their day to day activities.',
+    role: 'Senior Software Engineer',
+    techStack: ['Angular', 'Web API']
+  },
+  {
+    id: 9,
+    name: 'Amdocs Academy',
+    description:
+      'Amdocs Academy is an innovative cloud learning portal providing a personalized role and scenario-based learning experience that improves employee performance.',
+    role: 'Senior Software Engineer',
+    techStack: ['Angular Js', 'Web API']
+  },
+  {
+    id: 10,
+    name: 'Axalta Coating System',
+    description:
+      'Axalta is the second largest powder coating solution in the world. Integrated online system, manages scheduling, exacting, dispatching and other activities.',
+    role: 'Senior Software Engineer',
+    techStack: ['Angular Js', 'MVC', 'Web API']
+  },
+  {
+    id: 11,
+    name: 'erail.in',
+    description:
+      'Service oriented website exploring the trains of Indian Railways. Train route, seat availability and other features were the USP of the service.',
+    role: 'Software Engineer',
+    techStack: ['JQuery', 'Web Forms', 'SQL Compact']
+  }
+];
+
 const RecentProjects = () => {
-    const responsibilities = [
-        'Proactively research and locate necessary tools and processes to identify troublesome trends as they develop.',
-        'Introduce new IT system design and testing procedures as well as quality standards to improve business performance, productivity and compliance across organization.',
-        'Provide expert application design, guidance on solution system designs, redesigns for platform, performance and integration of new technical features and capabilities.',
-        'Write clean, maintainable and efficient code.',
-        'Participate in the architecture and design of the feature'
-    ];
+  return (
+    <div className='custom-list'>
+      <div className='custom-list-wrapper'>
+        <div className='header-title'>Most Recent Projects</div>
+        <div className='heading-separator'></div>
+        <div className='custom-list-box'>
+          {projectList.map((project) => {
+            return (
+              <div className='list-group recent-project-item' key={project.id}>
+                <div
+                  className='project-a list-group-item list-group-item-action'
+                  aria-current='true'
+                >
+                  <div className='row project-row'>
+                    <div className='col-lg-6 col-md-12 col-sm-12 recent-project-col'>
+                      <h5 className='mb-1 custom-list-item-title'>
+                        {project.name}
+                      </h5>
+                    </div>
 
-    const projectList = [
-        {
-            id: 1,
-            name: 'File Exchange - XR',
-            description: 'File Exchange leverages four decades of transmission and translation expertise to provide the most robust data delivery and transformation tool available. With File Exchage, banks can eliminate the hassles of installing software, managing servers, designing networks.',
-            role: 'UI Architect',
-            techStack: ['ReactJs', 'Redux'],
-            rolesAndResponsibilities: responsibilities
-        },
-        {
-            id: 2,
-            name: 'Payments Exchange - RTP',
-            description: 'With the use of RTP, banks can now rapidly onboard new customer while providing existing customer an expanded suite of payments options, including real time payments.',
-            role: 'UI Architect',
-            techStack: ['ReactJs', 'Redux']
-        },
-        {
-            id: 3,
-            name: 'Enterprise Payment Platform',
-            description: 'EPP UI is the modern UI for handling all types of payments. ',
-            role: 'UI Architect',
-            techStack: ['ReactJs', 'Redux']
-        },
-        {
-            id: 4,
-            name: 'CSCS',
-            description: 'CSCS cards provide proof that individuals working on construction sites have the appropriate training and qualifications for the job they do on site. By ensuring the workforce are appropriately qualified the card plays its part in improving standards and safety on UK construction sites',
-            role: 'Software Architect',
-            techStack: ['Angular 8', 'NodeJs', 'AWS']
-        },
-        {
-            id: 5,
-            name: 'ASTM',
-            description: 'ASTM International, formerly known as American Society for Testing and Materials, is an international standards organization that develops and publishes voluntary consensus technical standards for a wide range of materials, products, systems, and services.',
-            role: 'UI Architect',
-            techStack: ['ReactJs', 'NodeJs']
-        },
-        {
-            id: 6,
-            name: 'MyBI',
-            description: 'MyBI Portal is a one stop web application which allows AT&T staff to search documents shared across multiple source systems. Users can also subscribe any document and if that document has any change, it gets notified to the user.',
-            role: 'Tech Lead, UI Specialist',
-            techStack: ['ReactJs', 'NodeJs']
-        },
-        {
-            id: 7,
-            name: 'CityNet',
-            description: 'NLT is one of the biggest Internet Service provider of Singapore. Users can request for new connection with their convenient time, and then they can track the progress of that request.',
-            role: 'Tech Lead, UI Specialist',
-            techStack: ['ReactJs', 'Redux']
-        },
-        {
-            id: 8,
-            name: 'XPO Logistics',
-            description: 'XPO Logistics is one of the largest logistics companies of US. This product is to provide a high-end system to enhance their day to day activities.',
-            role: 'Senior Software Engineer',
-            techStack: ['Angular', 'Web API']
-        },
-        {
-            id: 9,
-            name: 'Amdocs Academy',
-            description: 'Amdocs Academy is an innovative cloud learning portal providing a personalized role and scenario-based learning experience that improves employee performance.',
-            role: 'Senior Software Engineer',
-            techStack: ['Angular Js', 'Web API']
-        },
-        {
-            id: 10,
-            name: 'Axalta Coating System',
-            description: 'Axalta is the second largest powder coating solution in the world. Integrated online system, manages scheduling, exacting, dispatching and other activities.',
-            role: 'Senior Software Engineer',
-            techStack: ['Angular Js', 'MVC', 'Web API']
-        },
-        {
-            id: 11,
-            name: 'erail.in',
-            description: 'Service oriented website exploring the trains of Indian Railways. Train route, seat availability and other features were the USP of the service.',
-            role: 'Software Engineer',
-            techStack: ['JQuery', 'Web Forms', 'SQL Compact']
-        }
-    ];
+                    <div className='col-lg-6 col-md-12 col-sm-12 project-stack float-end'>
+                      <small>
+                        {project.techStack.map((st) => {
+                          return (
+                            <span className='badge bg-secondary me-2' key={st}>
+                              {st}
+                            </span>
+                          );
+                        })}
+                      </small>
+                    </div>
+                  </div>
 
+                  <p className='mb-1'>
+                    <small className='custom-list-item-sub-title text-muted'>
+                      {' '}
+                      {project.description}
+                    </small>
+                  </p>
 
-    return (
-        <div className="custom-list">
-            <div className="custom-list-wrapper">
-                <div className="header-title">Most Recent Projects</div>
-                <div className="heading-separator"></div>
-                <div className="custom-list-box">
-                    {projectList.map((project) => {
-                        return <div className="list-group recent-project-item" key={project.id}>
+                  {project.rolesAndResponsibilities && (
+                    <div>
+                      <span className='mt-3 fw-bold'>
+                        Roles and Responsibilities include:
+                      </span>
 
-                            <div className="project-a list-group-item list-group-item-action" aria-current="true">
-                                <div className="row project-row">
+                      <span className='text-secondary'>
+                        <ol className='mb-3'>
+                          {project.rolesAndResponsibilities.map((resp) => {
+                            return <li key={resp}>{resp}</li>;
+                          })}
+                        </ol>
+                      </span>
+                    </div>
+                  )}
 
-                                    <div className="col-lg-6 col-md-12 col-sm-12 recent-project-col">
-                                        <h5 className="mb-1 custom-list-item-title">{project.name}</h5>
-                                    </div>
+                  <small>
+                    <span className='badge bg-primary me-2'>
+                      {project.role}
+                    </span>
+                  </small>
 
-                                    <div className="col-lg-6 col-md-12 col-sm-12 project-stack float-end">
-                                        <small>
-                                            {
-                                                project.techStack.map((st) => {
-                                                    return <span className="badge bg-secondary me-2" key={st}>{st}</span>
-                                                })
-                                            }
-                                        </small>
-                                    </div>
-                                </div>
-
-                                <p className="mb-1">
-                                    <small className="custom-list-item-sub-title text-muted"> {project.description}</small>
-                                </p>
-
-                                {project.rolesAndResponsibilities && <div>
-
-                                    <span className="mt-3 fw-bold">
-                                        Roles and Responsibilities include:
-                                    </span>
-
-                                    <span className="text-secondary">
-                                        <ol className="mb-3">
-                                            {project.rolesAndResponsibilities.map((resp) => {
-                                                return <li key={resp}>{resp}</li>;
-                                            })
-                                            }
-                                        </ol>
-                                    </span>
-
-                                </div>
-                                }
-
-                                <small>
-                                    <span className="badge bg-primary me-2">{project.role}</span>
-                                </small>
-                            </div>
-
-
-
-
-                        </div>
-                    })}
+                  {project.link && (
+                    <>
+                      <small className='project-link'>
+                        <a
+                          href={project.link}
+                          target='_blank'
+                          className='bg-link me-2'
+                        >
+                          {project.link}
+                        </a>
+                      </small>
+                    </>
+                  )}
                 </div>
-            </div>
+              </div>
+            );
+          })}
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default RecentProjects;
--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -1,168 +1,191 @@
-import reactImg from '../../assets/react.png';
-import angularImg from '../../assets/angular.png';
-import nodeJsImg from '../../assets/node.png';
-import expressJsImg from '../../assets/express.png';
-import mongoDBImg from '../../assets/mongodb.png';
-import javascriptImg from '../../assets/javascript.png';
-import typescriptImg from '../../assets/typescript.png';
-import dotnetImg from '../../assets/dotnet.png';
-import jqueryImg from '../../assets/jquery.png';
-
-import './skill.css';
 import { useEffect, useState } from 'react';
+import angularImg from '../../assets/angular.png';
+import cssImg from '../../assets/css.png';
+import expressJsImg from '../../assets/express.png';
+import javascriptImg from '../../assets/javascript.png';
+import jqueryImg from '../../assets/jquery.png';
+import mongoDBImg from '../../assets/mongodb.png';
+import nodeJsImg from '../../assets/node.png';
+import reactImg from '../../assets/react.png';
+import typescriptImg from '../../assets/typescript.png';
+import './skill.css';
 
 const SkillList = () => {
-    const skillList = [
-        {
-            id: 1,
-            name: 'React Js + Redux',
-            icon: reactImg,
-            duration: 6,
-            since: 2015,
-            exp: 0,
-            percent: 0
-        },
-        {
-            id: 2,
-            name: 'Angular 2+',
-            icon: angularImg,
-            duration: 6,
-            since: 2015,
-            exp: 0,
-            percent: 0
-        },
-        {
-            id: 3,
-            name: 'Node Js',
-            icon: nodeJsImg,
-            duration: 8,
-            since: 2013,
-            exp: 0,
-            percent: 0
-        },
-        {
-            id: 4,
-            name: 'JavaScript',
-            icon: javascriptImg,
-            duration: 11,
-            since: 2010,
-            exp: 0,
-            percent: 0
-        },
-        {
-            id: 5,
-            name: 'TypeScript',
-            icon: typescriptImg,
-            duration: 4,
-            since: 2015,
-            exp: 0,
-            percent: 0
-        },
-        {
-            id: 6,
-            name: 'JQuery',
-            icon: jqueryImg,
-            duration: 10,
-            since: 2011,
-            exp: 0,
-            percent: 0
-        },
-        {
-            id: 7,
-            name: 'Express Js',
-            icon: expressJsImg,
-            duration: 8,
-            since: 2013,
-            exp: 0,
-            percent: 0
-        },
-        {
-            id: 8,
-            name: 'MongoDB',
-            icon: mongoDBImg,
-            duration: 5,
-            since: 2015,
-            exp: 0,
-            percent: 0
-        },
-        {
-            id: 9,
-            name: '.NET',
-            icon: dotnetImg,
-            duration: 6,
-            since: 2015,
-            exp: 0,
-            percent: 0
-        }
-    ];
-
-    const careerStartedFrom = 2010;
-
-    const [skillsList, setSkillList] = useState(skillList);
-
-    const calculateExp = (since) => {
-        const dt2 = new Date();
-        const dt1 = new Date(since, 1, 1);
-        return calculateGapBetweenTwoDates(dt2, dt1);
+  const skillList = [
+    {
+      id: 1,
+      name: 'React Js + Redux',
+      icon: reactImg,
+      duration: 6,
+      since: 2015,
+      exp: 0,
+      percent: 0
+    },
+    {
+      id: 2,
+      name: 'Angular 2+',
+      icon: angularImg,
+      duration: 6,
+      since: 2015,
+      exp: 0,
+      percent: 0
+    },
+    {
+      id: 3,
+      name: 'Node Js',
+      icon: nodeJsImg,
+      duration: 8,
+      since: 2013,
+      exp: 0,
+      percent: 0
+    },
+    {
+      id: 4,
+      name: 'JavaScript',
+      icon: javascriptImg,
+      duration: 11,
+      since: 2010,
+      exp: 0,
+      percent: 0
+    },
+    {
+      id: 5,
+      name: 'TypeScript',
+      icon: typescriptImg,
+      duration: 4,
+      since: 2015,
+      exp: 0,
+      percent: 0
+    },
+    {
+      id: 6,
+      name: 'JQuery',
+      icon: jqueryImg,
+      duration: 10,
+      since: 2011,
+      exp: 0,
+      percent: 0
+    },
+    {
+      id: 7,
+      name: 'Express Js',
+      icon: expressJsImg,
+      duration: 8,
+      since: 2013,
+      exp: 0,
+      percent: 0
+    },
+    {
+      id: 8,
+      name: 'MongoDB',
+      icon: mongoDBImg,
+      duration: 5,
+      since: 2015,
+      exp: 0,
+      percent: 0
+    },
+    {
+      id: 8,
+      name: 'CSS/SCSS',
+      icon: cssImg,
+      duration: 5,
+      since: 2015,
+      exp: 0,
+      percent: 0
     }
 
-    const calculateGapBetweenTwoDates = (dt2, dt1) => {
-        let diff = (dt2.getTime() - dt1.getTime()) / 1000;
-        diff /= (60 * 60 * 24);
-        return Math.abs(Math.round(diff / 365.25));
-    }
+    // {
+    //   id: 9,
+    //   name: '.NET',
+    //   icon: dotnetImg,
+    //   duration: 6,
+    //   since: 2018,
+    //   exp: 0,
+    //   percent: 0
+    // }
+  ];
 
-    const calculatePercent = (exp) => {
-        const dt2 = new Date();
-        const dt1 = new Date(careerStartedFrom, 1, 1);
-        const totalExp = calculateGapBetweenTwoDates(dt2, dt1);
-        return Math.abs(Math.round((exp / totalExp) * 100));
-    }
+  const careerStartedFrom = 2010;
 
-    const updateSkillSetArray = () => {
-        skillsList.map((skill) => {
-            const yearGap = calculateExp(skill.since);
-            skill.exp = yearGap;
-            skill.percent = calculatePercent(yearGap);
-            return skill;
-        });
+  const [skillsList, setSkillList] = useState(skillList);
 
-        setSkillList([...skillsList])
-    }
+  const calculateExp = (since) => {
+    const dt2 = new Date();
+    const dt1 = new Date(since, 1, 1);
+    return calculateGapBetweenTwoDates(dt2, dt1);
+  };
 
-    useEffect(() => {
-        updateSkillSetArray();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+  const calculateGapBetweenTwoDates = (dt2, dt1) => {
+    let diff = (dt2.getTime() - dt1.getTime()) / 1000;
+    diff /= 60 * 60 * 24;
+    return Math.abs(Math.round(diff / 365.25));
+  };
 
-    return (
-        <div className="custom-list">
-            <div className="custom-list-wrapper">
-                <div className="header-title">Skills</div>
-                <div className="heading-separator"></div>
-                <div className="custom-list-box">
-                    {skillsList.map((skill) => {
-                        return <div className="custom-list-item" key={skill.id}>
-                            <div className="col-md-2 align-middle">
-                                {skill.icon && <img src={skill.icon} className="skill-icon rounded-circle" alt={skill.name} />}
-                            </div>
-                            <div className="col-md-10 align-middle">
+  const calculatePercent = (exp) => {
+    const dt2 = new Date();
+    const dt1 = new Date(careerStartedFrom, 1, 1);
+    const totalExp = calculateGapBetweenTwoDates(dt2, dt1);
+    return Math.abs(Math.round((exp / totalExp) * 100));
+  };
 
-                                <div className="custom-list-item-title">{skill.name}
-                                </div>
-                                <div className="custom-list-item-sub-title">
-                                    <div className="progress">
-                                        <div className="progress-bar" role="progressbar" style={{ width: `${skill.percent}%` }}
-                                            aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">{skill.exp}+ years</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    })}
+  const updateSkillSetArray = () => {
+    skillsList.map((skill) => {
+      const yearGap = calculateExp(skill.since);
+      skill.exp = yearGap;
+      skill.percent = calculatePercent(yearGap);
+      return skill;
+    });
+
+    setSkillList([...skillsList]);
+  };
+
+  useEffect(() => {
+    updateSkillSetArray();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className='custom-list'>
+      <div className='custom-list-wrapper'>
+        <div className='header-title'>Skills</div>
+        <div className='heading-separator'></div>
+        <div className='custom-list-box'>
+          {skillsList.map((skill) => {
+            return (
+              <div className='custom-list-item' key={skill.id}>
+                <div className='col-md-2 align-middle'>
+                  {skill.icon && (
+                    <img
+                      src={skill.icon}
+                      className='skill-icon rounded-circle'
+                      alt={skill.name}
+                    />
+                  )}
                 </div>
-            </div>
-        </div >
-    );
-}
+                <div className='col-md-10 align-middle'>
+                  <div className='custom-list-item-title'>{skill.name}</div>
+                  <div className='custom-list-item-sub-title'>
+                    <div className='progress'>
+                      <div
+                        className='progress-bar'
+                        role='progressbar'
+                        style={{ width: `${skill.percent}%` }}
+                        aria-valuenow='25'
+                        aria-valuemin='0'
+                        aria-valuemax='100'
+                      >
+                        {skill.exp}+ years
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SkillList;
